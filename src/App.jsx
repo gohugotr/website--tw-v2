748cb3593a9bdf70b92256ce594d94a182512afb
Cengiz YILMAZ <terabilgi@gmail.com>
2022-06-12 02:05:17 +0300

    Content ve css tamamlandı

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Content from "./components/Content"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
@@ -7,6 +8,7 @@ const App = () => {
     <>
       <Navbar/>
       <Hero/>
+      <Content/>
     </>
   )
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import ImageOne from '../images/yumurta.jpg'
+import ImageTwo from '../images/yumurta1.jpg'
 
 const Content = () => {
   return (
-    <div>Content</div>
+    <>
+      <div className='menu-card'>
+        <img src={ImageOne} alt='Yumurta' className='h-full mb-20 rounded shadow' />
+        <div className='center-content'>
+          <h2 className='mb-2 text-2xl'>Tane Tane Yumurta</h2>
+          <p className='mb-2'>Doğal, organik ve taptaze yumurtalar</p>
+          <strong>16₺</strong>
+        </div>
+      </div>
+      <div className='menu-card'>
+        <img src={ImageTwo} alt='Yumurta2' className='h-full mb-20 rounded shadow' />
+        <div className='center-content'>
+          <h2 className='mb-2 text-2xl'>Çift Sarılı Yumurta</h2>
+          <p className='mb-2'>Doğal, organik ve taptaze yumurtalar</p>
+          <strong>22₺</strong>
+        </div>
+      </div>
+    </>
   )
 }
 
