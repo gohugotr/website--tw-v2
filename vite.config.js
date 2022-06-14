3608599e10564f4e923f3f25eb02ac46a12adbb5
Cengiz YILMAZ <terabilgi@gmail.com>
2022-06-15 00:05:29 +0300

    vite.config düzenlendi

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/website-tw-v2/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      $: path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/website-tw-v2/',
+  publicDir: 'public',
 })
